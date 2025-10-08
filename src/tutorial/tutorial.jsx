4e12b428c81129a5edf314c93fcab4f8205ef1f4
reecegeese <reece.d.loveridge@gmail.com>
2025-10-08 17:11:49 -0600

    made all pages more responsive to smaller screens

--- src/tutorial/tutorial.jsx
+++ src/tutorial/tutorial.jsx
@@ -5,12 +5,12 @@ export function Tutorial() {
   return (
     <main className="container-fluid text-center">
       <div>
-        <main class="container-fluid text-center pt-5 pb-5">
+        <main className="container-fluid text-center pt-5 pb-5">
           <div id="picture" class="picture-box">
             <img src="Staring Cat.PNG" alt="random cat" />
           </div>
 
-          <p class="w-50">
+          <p className="w-50">
             List maker is the perfect way for any group to easily collect and
             complete a list of anything. From grocery shopping to household
             chorse, list maker has got it covered. Just create your group, add
@@ -19,17 +19,17 @@ export function Tutorial() {
             who crossed something off
           </p>
 
-          <p class="w-50">
+          <p className="w-50">
             I could easily copyright this idea and make a billion dollars out of
             law suits and retire in my mansion on the moon. Fortunatly for you
             i'm a nice person so this site is COMPLETLY FREE. Lucky you.
           </p>
 
-          <p class="w-50">
+          <p className="w-50">
             That cat staring at you is hard coded but will eventually be
             randomized and show you a new picture every time you open this page
           </p>
-          <p class="w-50">
+          <p className="w-50">
             And this line will do the same with a random quote or fact or
             somthing. I don't know yet. I'll see what's funny and pick that
           </p>
