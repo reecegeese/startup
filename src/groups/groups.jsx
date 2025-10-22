91748b1c1074688b9e56ca6b4bdb7847cea69a18
reecegeese <reece.d.loveridge@gmail.com>
2025-10-22 16:25:08 -0600

    reactivity for groups page

--- src/groups/groups.jsx
+++ src/groups/groups.jsx
@@ -1,40 +1,12 @@
 import React from "react";
 import "./groups.css";
 
-export function Groups() {
+import { Contributors } from "./contributors";
+
+export function Groups(props) {
   return (
     <main className="container-fluid text-center">
-      <div>
-        <main class="container-fluid justify-content-center">
-          <h1>Your Lists</h1>
-          <table class="custom-bordered-table">
-            <thead>
-              <tr>
-                <th>List Name</th>
-                <th>Total Items</th>
-                <th>Date Created</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Breakfast</td>
-                <td>8</td>
-                <td>June 14, 2023</td>
-              </tr>
-              <tr>
-                <td>Birthday Suprise</td>
-                <td>19</td>
-                <td>October 7, 2019</td>
-              </tr>
-              <tr>
-                <td>Living Room</td>
-                <td>4</td>
-                <td>Janurary 30, 2024</td>
-              </tr>
-            </tbody>
-          </table>
-        </main>
-      </div>
+      <Contributors userName={props.userName} />
     </main>
   );
 }
