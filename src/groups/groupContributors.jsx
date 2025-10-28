c8750ff120b368f03af94efe2d29deed10db57f7
reecegeese <reece.d.loveridge@gmail.com>
2025-10-28 14:45:11 -0600

    made groups page next white

--- src/groups/groupContributors.jsx
+++ src/groups/groupContributors.jsx
@@ -10,11 +10,7 @@ export function GroupContributors(props) {
     <div className="contributors">
       <h1>{userName}'s lists</h1>
       <div id="contributor-messages">
-        {props.list.length > 0 && (
-          <p className="list-full-message text-warning">
-            {userName} has 1 list
-          </p>
-        )}
+        {props.list.length > 0 && <p>{userName} has 1 list</p>}
       </div>
     </div>
   );
