7a21ece19dca2af8f6db61c5c2eaf2a7954de674
reecegeese <reece.d.loveridge@gmail.com>
2025-10-27 20:44:52 -0600

    re added about page and fixed footer test colors

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import Button from 'react-bootstrap/Button';
-
-import './authenticated.css';
+import Button from "react-bootstrap/Button";
+import "./login.css";
 
 export function Authenticated(props) {
   const navigate = useNavigate();
 
   function logout() {
-    localStorage.removeItem('userName');
+    localStorage.removeItem("userName");
     props.onLogout();
   }
 
   return (
     <div>
-      <div className='playerName'>{props.userName}</div>
-      <Button variant='primary' onClick={() => navigate('/play')}>
-        Play
+      <div className="playerName">{props.userName}</div>
+      <Button
+        variant="primary"
+        id="login_button"
+        onClick={() => navigate("/play")}
+      >
+        Create List
       </Button>
-      <Button variant='secondary' onClick={() => logout()}>
+      <Button variant="secondary" id="create_button" onClick={() => logout()}>
         Logout
       </Button>
     </div>
