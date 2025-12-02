f65588c0475bedb053d42538b781d59c3ae986f3
reecegeese <reece.d.loveridge@gmail.com>
2025-12-02 10:05:51 -0700

    fixed website not loading

--- service/database.js
+++ service/database.js
@@ -3,9 +3,8 @@ const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db("startup");
+const db = client.db("simon");
 const userCollection = db.collection("user");
-const scoreCollection = db.collection("score");
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -34,6 +33,20 @@ async function addUser(user) {
 
 async function updateUser(user) {
   await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addScore(score) {
+  return scoreCollection.insertOne(score);
+}
+
+function getHighScores() {
+  const query = { score: { $gt: 0, $lt: 900 } };
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
 }
 
 module.exports = {
