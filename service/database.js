2704b9c3cccf1b915a2a14c06975c1041898ed1f
reecegeese <reece.d.loveridge@gmail.com>
2025-12-02 10:07:54 -0700

    fixed endpoints

--- service/database.js
+++ service/database.js
@@ -3,7 +3,7 @@ const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db("simon");
+const db = client.db("startup");
 const userCollection = db.collection("user");
 
 // This will asynchronously test the connection and exit the process if it fails
