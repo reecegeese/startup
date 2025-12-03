2e276e4c90ad24401de8ae1d9ddb74a6ed636fda
reecegeese <reece.d.loveridge@gmail.com>
2025-12-03 16:18:47 -0700

    added cross user websocket messages to lists page

--- src/lists/notifier.js
+++ src/lists/notifier.js
@@ -15,17 +15,27 @@ class EventNotifier {
   handlers = [];
 
   constructor() {
-    // Simulate chat messages that will eventually come over WebSocket
-    setInterval(() => {
-      const score = Math.floor(Math.random() * 3000);
-      const date = new Date().toLocaleDateString();
-      const userName = "Berners-Lee";
-      this.broadcastEvent(userName, Event.End, {
-        name: userName,
-        score: score,
-        date: date,
-      });
-    }, 5000);
+    let port = window.location.port;
+    const protocol = window.location.protocol === "http:" ? "ws" : "wss";
+    this.socket = new WebSocket(
+      `${protocol}://${window.location.hostname}:${port}/ws`
+    );
+    this.socket.onopen = (event) => {
+      this.receiveEvent(
+        new EventMessage("Simon", GameEvent.System, { msg: "connected" })
+      );
+    };
+    this.socket.onclose = (event) => {
+      this.receiveEvent(
+        new EventMessage("Simon", GameEvent.System, { msg: "disconnected" })
+      );
+    };
+    this.socket.onmessage = async (msg) => {
+      try {
+        const event = JSON.parse(await msg.data.text());
+        this.receiveEvent(event);
+      } catch {}
+    };
   }
 
   broadcastEvent(from, type, value) {
